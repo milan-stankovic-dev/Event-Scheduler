b68af78fcb3618676d12b56120429dc267f70990
milan-stankovic-dev <milance.za019@gmail.com>
2025-03-07 09:13:00 +0100

    Removed DBConfig class from the project and re-introduced its sole method into DBCredentials to make code more concise and easier to understand. Added code comments in LiquibaseIntegration's runLiquibaseScripts to explain the disabling of liquibase logging.

--- src/main/java/org/example/db/DBCredentials.java
+++ src/main/java/org/example/db/DBCredentials.java
@@ -1,7 +1,10 @@
 package org.example.db;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
+import lombok.val;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,22 +22,34 @@ public class DBCredentials {
      */
     private DBCredentials() { }
     /**
-     * Extracted config values, in the form of a map.
-     */
-    private final Map<String,String> CONFIG = DBConfig.getInstance().getDotEnvVars();
-    /**
      * Url constant
      */
     @Getter
-    private final String URL = CONFIG.get("url");
+    private final String URL = getDotEnvVars().get("url");
     /**
      * Username constant
      */
     @Getter
-    private final String USER = CONFIG.get("user");
+    private final String USER = getDotEnvVars().get("user");
     /**
      * Password constant
      */
     @Getter
-    private final String PASS = CONFIG.get("pass");
+    private final String PASS = getDotEnvVars().get("pass");
+    /**
+     * Extracts environment variables for db connection
+     * @return Map of three entries. The keys include:
+     *  * url - database url
+     *  * user - database username
+     *  * pass - database password
+     */
+    private Map<String, String> getDotEnvVars() {
+        val dotEnv = Dotenv.load();
+
+        return new HashMap<>() {{
+            put("url", dotEnv.get("DB_URL"));
+            put("user", dotEnv.get("DB_USER"));
+            put("pass", dotEnv.get("DB_PASS"));
+        }};
+    }
 }
